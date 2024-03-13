49014b003ebf2a11561a0613f136ed584536df36
chetanbnagmotisaveeat <chetan.nagmoti@saveeat.in>
2024-03-13 13:11:26 +0530

    Completed Gallry

--- src/Helpers/SwiperComponet.jsx
+++ src/Helpers/SwiperComponet.jsx
@@ -10,65 +10,62 @@ import 'swiper/css/navigation';
 
 import '../styles/swiperStyle.css';
 
+const imagesArrya = [
+  { id: 1, url: 'https://swiperjs.com/demos/images/nature-1.jpg' },
+  { id: 2, url: 'https://swiperjs.com/demos/images/nature-2.jpg' },
+  { id: 3, url: 'https://swiperjs.com/demos/images/nature-3.jpg' },
+  { id: 4, url: 'https://swiperjs.com/demos/images/nature-4.jpg' },
+  { id: 5, url: 'https://swiperjs.com/demos/images/nature-5.jpg' },
+  { id: 6, url: 'https://swiperjs.com/demos/images/nature-3.jpg' },
+  { id: 7, url: 'https://swiperjs.com/demos/images/nature-4.jpg' },
+  { id: 8, url: 'https://swiperjs.com/demos/images/nature-5.jpg' },
+]
+
 // import required modules
-import {Autoplay, EffectCoverflow, Pagination,Navigation } from 'swiper/modules';
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 const SwiperComponet = () => {
   return (
     <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        // pagination={{
-        //   clickable: true,
-        // }}
-        // navigation={true}
-        modules={[Autoplay,EffectCoverflow, Pagination,Navigation]}
-        className="mySwiper "
-      >
-        <div  style={{margin:"50px"}}>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img  src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        </div>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide style={{margin:"50px"}}>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-      </Swiper>
+      effect={'coverflow'}
+      grabCursor={true}
+      centeredSlides={true}
+      slidesPerView={'auto'}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      loop={true}
+      // pagination={{
+      //   clickable: true,
+      // }}
+      // navigation={true}
+      modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+      className="mySwiper "
+    >
+      <div style={{ margin: "50px" }}>
+        {
+          imagesArrya.map((image, index) => {
+            return (
+              <>
+                <SwiperSlide key={image.id+index} style={{ margin: "50px" }}>
+                  <img src={image.url} />
+                </SwiperSlide>
+              </>
+            )
+          })
+        }
+
+      </div>
+
+    </Swiper>
   )
 }
 
--- src/componets/Home_Componets/Gallary_Frame.jsx
+++ src/componets/Home_Componets/Gallary_Frame.jsx
@@ -4,25 +4,95 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
-import '../../styles/Home.css'
 import SwiperComponet from '../../Helpers/SwiperComponet';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../../styles/swiperStyle.css';
+import '../../styles/Gallary_Frame.css'
+
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
 
 const Gallary_Frame = () => {
+
+  const imagesArray = [
+    { id: 1, url: '../images/Curoals/Image-1.png' },
+    { id: 2, url: '../images/Curoals/Image-2.png' },
+    { id: 3, url: '../images/Curoals/Image-3.png' },
+    { id: 4, url: '../images/Curoals/Image-4.png' },
+    { id: 5, url: '../images/Curoals/Image-5.png' },
+    { id: 6, url: '../images/Curoals/Image-2.png' },
+    { id: 7, url: '../images/Curoals/Image-3.png' },
+    { id: 8, url: '../images/Curoals/Image-4.png' },
+  ];
+
   return (
     <ThemeProvider
-    breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-    minBreakpoint="xxs"
-  >
-    <Container fluid className='p-0 m-0 position-relative  w-100'>
-      <Row className='p-0 m-0 '>
-        <Col xs={12} md={12} lg={12} className="position-relative p-0 m-0" >
-        <SwiperComponet /> 
-        </Col>
+      breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+      minBreakpoint="xxs"
+    >
+      <Container fluid className='position-relative overflow-hidden w-100 border border-1 border-primary' >
+        <div className='home-main-div-margin-bottom-and-top'>
+
+      <Row className="p-0 m-0 ">
+            <Col className="home-heading home-main-div-margin-bottom-and-top  border border-1 border-parimary">
+              Gallery
+            </Col>
       </Row>
-    </Container>
+        <Row className='p-0 m-0 w-100 ' >
+          <Col xs={12} md={12} lg={12} className="position-relative w-100 " >
+            <Swiper
+              effect={'coverflow'}
+              grabCursor={true}
+              centeredSlides={true}
+              slidesPerView={'auto'} // Adjust number of visible slides automatically
+              coverflowEffect={{
+                rotate: 0,
+                stretch: -30,
+                depth: 50,
+                modifier: 4,
+                slideShadows: true,
+              }}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              pagination={true}
+              loop={true}
+              modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+              className="mySwiper border border-1 border-warning mt-2 mb-2"
+              style={{ width: '100%', height: 'auto' }}
+            >
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                {imagesArray.map((image, index) => (
+                  <SwiperSlide key={image.id + index} style={{ margin: "10px" }}> {/* Adjust margin for spacing */}
+                    <img
+                      src={image.url}
+                      alt={`Image ${image.id}`}
+                      style={{
+                        objectFit: 'cover',
+                        maxWidth: '100%', // Ensure image doesn't exceed container width
+                        height: 'auto',  // Maintain aspect ratio
+                        borderRadius: '25px',
+                      }}
+                    />
+                  </SwiperSlide>
+                ))}
+              </div>
+            </Swiper>
 
-  </ThemeProvider>
-  )
-}
 
-export default Gallary_Frame
+          </Col>
+        </Row>
+        </div>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default Gallary_Frame;
